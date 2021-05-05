929e1d6f98b228e2b1143d9e93b2013e71654352
Megha Agarwal <meghaagrwal.18@gmail.com>
2021-05-05 16:59:34 +0530

    php

---
+++ include/header.php
@@ -0,0 +1,76 @@
+<header class="myhead">
+      <div class="container">
+        <div class="row head">
+          <div class="col-sm-5 head-text" style="text-align: left;">
+            <h2 style="color:white;">Central Library</h2> 
+            <h4 style="color:white;">National Institute of Technology, Jamshedpur</h4>
+          </div>
+           <div class="col-sm-1">
+            <img class="logo" src="image/logo.png">
+          </div>
+          <div class="col-sm-5 head-text" style="text-align:right">
+            <h2 style="color:white;">केंद्रीय पुस्तकालय</h2> 
+            <h4 style="color:white;">   राष्ट्रीय प्रौद्योगिकी संस्थान, जमशेदपुर</h4>
+          </div>
+        </div>
+      </div>
+
+
+  <!--  nav part -->
+<nav  class="navbar navbar-inverse" data-spy="affix" data-offset-top="197" style="background-color:#030218; ">
+ <div class="container-fluid container ">
+
+    <ul class="nav navbar-nav">
+      <li class="tabOuter" ><a class="borderOuter" href="#">Home</a></li>
+      <li class="tabOuter" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">About<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a class="borderInner" href="#">Library Team</a></li>
+          <li><a class="borderInner" href="#">Working Hours</a></li>
+          <li><a class="borderInner" href="#">Rules</a></li>
+  <li><a class="borderInner" href="#">Space</a></li>
+  <li><a class="borderInner" href="#">Acquistion and Processing Team</a></li>
+  <li><a href="#">Photo Gallery</a></li>
+        </ul>
+      </li>
+<li class="tabOuter" class="dropdown"><a class="borderOuter" class="dropdown-toggle" data-toggle="dropdown" href="#">Services<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a class="borderInner" href="#">Information</a></li>
+          <li><a class="borderInner" href="#">Feedback</a></li>
+          <li><a class="borderInner" href="#">Book Bank</a></li>
+  <li><a href="#">Recommend a Book</a></li>
+  
+        </ul>
+      </li>
+<li class="tabOuter" class="dropdown"><a class="borderOuter" class="dropdown-toggle" data-toggle="dropdown" href="#">Resources<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a class="borderInner" href="#">E-TextBooks</a></li>
+          <li><a class="borderInner" href="#">Online-Journals</a></li>
+          <li><a  href="#">Magazines</a></li>
+  
+        </ul>
+      </li>
+      </ul>
+      
+   <ul class="nav navbar-nav navbar-right">
+      <li class="tabOuter" class="dropdown"><a class="borderOuter" class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span> Sign Up <span  class="caret"></span></a>
+          <ul class="dropdown-menu">
+          <li><a class="borderInner" href="#">Admin</a></li>
+          <li><a  href="#">Student</a></li>
+          </ul>
+          </li>
+          <li class="tabOuter" class="dropdown"><a class="borderOuter" class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-log-in"></span> Login <span  class="caret"></span></a>
+          <ul class="dropdown-menu">
+          <li><a class="borderInner" href="#">Admin</a></li>
+          <li><a  href="#">Student</a></li>
+          </ul>
+          </li>
+     <!--<li class="dropdown"><a  class="dropdown-toggle" data-toggle="dropdowm" href="#"><span class="glyphicon glyphicon-log-in" class="caret"></span> Login</a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Admin</a></li>
+          <li><a href="#">Student</a></li>
+          </ul>
+          </li>-->
+    </ul>
+  </div>
+</nav>
+</header>
