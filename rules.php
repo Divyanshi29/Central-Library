cf7462a28b1b9a16e724bc1b6135d2f2f9d0f7f5
unknown <abhilashapathak999@gmail.com>
2021-05-11 19:18:45 +0530

    old updated

---
+++ rules.php
@@ -0,0 +1,34 @@
+<?php include("include/header.php");
+include("include/main.php");
+?>
+<h2 style="margin-left:150px;font-family:Times New Roman;padding:18px 5px 5px 5px;"><b>General Library Rules</b></h2>
+<div class="container" style="text-align:left; font-size:18px;margin-left:150px;padding:20px 50px 150px 50px;">
+<div class="row">
+<ul>
+<li>All the students/scholars and outsiders entering the Library shall deposit their bags and other belongings at the entrance and sign in the Register at the checkpoint.</li>
+<li>Only notebooks and the Library books to be returned will be allowed inside.</li>
+<li>All the readers are advised not to leave their valuables at the Check Point.Library is not responsible for any loss of personal belongings.</li>
+<li>All files, books and notebooks must be presented to the security guard at the checkpoint for inspection while leaving the Library. Library does not permit any exception in the observance of this rule.</li>
+<li>ID Card should be compulsory entering into the library.</li>
+<li>Books Borrowed should be protected from RAIN, DUST, INSECT, etc.</li>
+<li>Books removed from the shelves by students, if not required for reference, should be kept on the book trolley or on table nearest to them. Please do not try to shelve them yourself. Please remember that a book misplaced is a book lost.</li>
+<li>The newspaper(s) should be folded properly after reading and kept back in the designated place.</li>
+<li>Readers should not deface, mark, cut, mutilate or damage library resources in any way. If anyone is found doing so, he will be charged the full replacement cost of the resource.</li>
+<li>Borrowers, faculty, staff going on the leave with or without salary, deputation, study leave will have to return all borrowed material before leaving NITK Surathkal.</li>
+<li>The “PASS OUT” slip should be handed over to the security guard at the checkpoint before taking out the issued book/s.</li>
+<li>There will be a fine of Rs. 1.00 per General Shelf book per day.</li>
+<li>For the loss of Library Card, a fine of Rs.20/- each will be charged.</li>
+<li>Students are advised not to issue Books to others on their names. Reference Documents like Dictionary, Encyclopedia, Year Books, Journals (Loose & Bound), Theses, Project Reports, Census, Handbooks will not be issued out.</li>
+<li>Conversation and discussion disturbs library ambience. Therefore, all are requested to maintain dignified silence. If discussion is necessary, the common room should be utilized for the same.</li>
+<li>Smoking is not permitted in the Library.</li>
+<li>All users are requested to keep their mobiles switched off in the Library.</li>
+<li>Beverages and Eatables are not allowed inside the library.</li>
+<li>No visitor or guest is permitted to use the Library without the prior permission of the Librarian. He/She is required to produce a proper introduction letter from the concerned Institution/Organization to which he/she is attached.</li>
+<li>No photograph of the Library shall be taken without the prior permission of the Librarian.</li>
+<li>The Librarian reserves the right to call back any issued book/item at any time.</li>
+<li>All students are advised to come to the Library in decent dress as they are in the classrooms. Wearing Bermuda & strip tops are not allowed in the Library.</li>
+<li>Demand and suggestion slips are available at the circulation desk for your use.</li>
+</ul>
+</div>
+</div>
+<?php include("include/footerstrip.php"); ?>
