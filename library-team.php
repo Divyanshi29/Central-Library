8e24b514ba18649b9591514cf9e0716097444802
kapil484 <kapil.kr909@gmail.com>
2021-07-05 23:03:10 +0530

    Library team in user mode

--- library-team.php
+++ library-team.php
@@ -3,243 +3,151 @@
 include("include/main.php");
 include("include/header.php");
 ?>
-<section>
+<section style="margin-bottom: 20px;">
   <div class="container" style="box-shadow: 2px 2px 10px black;">
     <div class="row">
       <div class="col">
         <h3 style="text-align: center;font-weight: bolder;">Library Team</h3>
       </div>
     </div>
-    <div class="row">
-      <h4 style="padding: 15px 5px 10px 20px; font-weight: bolder;background-color: #cecece;">Library Committee</h4>
-      <div class="col" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Prof. A.M. Tigga</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">P/I Library,Chairman</p>
-          </div>
-        </div>
-      </div>
-    </div>
+      <?php
+        include('comman/connect.php');
+        $sql1 = "SELECT distinct teamtype FROM libraryteam order by(teamtype)";
+        $res1=$con->query($sql1);
+        while($row1=mysqli_fetch_array($res1))
+        { $team=$row1['teamtype'];
+          echo "<h4 style='padding: 15px 5px 10px 20px; font-weight: bolder;background-color: #cecece;'>".$row1['teamtype']."</h4>";
+          $sql = "SELECT * FROM libraryteam where teamtype='$team' order by(memberid)";
+          $res=$con->query($sql);
+          $i=1;
+          $f=0;
+          while($row=mysqli_fetch_array($res))
+          { $pic=$row['pic'];
+            if($i==1){
+              echo "<div class='row'>";
+              echo "<div class='col-sm-12' style='padding: 30px;'>";
+              echo "<div class='team'>";
+              echo "<div class='team-img'>";
+              echo "<img style='border-radius: 100%;' src="."image/Library-Team/".$pic.">";
+              echo "</div>";
+              echo "<div class='mname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['name']."</p>";
+              echo "</div>";
+              echo "<div class='pname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['teampost']."</p>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+            }
+            elseif($i==2||$i==3){
+              if($f==0 && $i==2){
+                $f=1;
+              echo "<div class='row'>";
 
+              echo "<div class='col-sm-6' style='padding: 30px;'>";
+              echo "<div class='team'>";
+              echo "<div class='team-img'>";
+              echo "<img style='border-radius: 100%;' src="."image/Library-Team/".$pic.">";
+              echo "</div>";
+              echo "<div class='mname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['name']."</p>";
+              echo "</div>";
+              echo "<div class='pname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['teampost']."</p>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+              }else{
+              echo "<div class='col-sm-6' style='padding: 30px;'>";
+              echo "<div class='team'>";
+              echo "<div class='team-img'>";
+              echo "<img style='border-radius: 100%;' src="."image/Library-Team/".$pic.">";
+              echo "</div>";
+              echo "<div class='mname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['name']."</p>";
+              echo "</div>";
+              echo "<div class='pname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['teampost']."</p>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
 
-    <div class="row">
-      <div class="col-sm-6" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Prof. R.N. Mohanty</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Convener</p>
-          </div>
-        </div>
-      </div>
+              echo "</div>";
+              $f=0;
+              }
+            }
+            else{
+              if($i%4==0 && $f==0){
+                $f=1;
+                echo "<div class='row'>";
 
-      <div class="col-sm-6" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Sri. N.K. Sethy</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Convener</p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Prof. D.K. Yadav</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Member</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-3" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Dr. Akhliesh Kumar</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Member</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-3" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Dr. Dilip Kumar</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Member</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-3" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Sri. S.K. Prasad</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Member</p>
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-    
-    <div class="row">
-      <h4 style="padding: 15px 5px 10px 20px; font-weight: bolder;background-color: #cecece;">Library Staffs</h4>
-      <div class="col" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Dr. Neeta Bharti</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Assistant Librarian</p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-    <div class="row">
-      <div class="col-sm-6" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Mr. N.K. Sethy</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Assistant Librarian</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-6" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Mr. Umesh Kumar</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Assistant Librarian</p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Mr. Ganesh Shrestha</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Typist Clerk</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-3" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Mr. Ravi Mahato</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Typist Clerk</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-3" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Mr. Saurabh Saini</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Typist Clerk</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-sm-3" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Mr. Rakesh Kumar</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Typist Clerk</p>
-          </div>
-        </div>
-      </div>
-
-    </div>
-    
-    <div class="row">
-      <div class="col-sm-3" style="padding: 30px;">
-        <div class="team">
-          <div class="team-img">
-            <img src="image/Library-Team/team-image.png">
-          </div>
-          <div class="mname">
-            <p style="font-family: 'Montserrat', sans-serif;">Mrs. Sunaina Devi</p>
-          </div>
-          <div class="pname">
-            <p style="font-family: 'Montserrat', sans-serif;">Typist Clerk</p>
-          </div>
-        </div>
-      </div>
-    </div>
+              echo "<div class='col-sm-3' style='padding: 30px;'>";
+              echo "<div class='team'>";
+              echo "<div class='team-img'>";
+              echo "<img style='border-radius: 100%;' src="."image/Library-Team/".$pic.">";
+              echo "</div>";
+              echo "<div class='mname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['name']."</p>";
+              echo "</div>";
+              echo "<div class='pname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['teampost']."</p>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+              }
+              elseif($i%4==1){
+                echo "<div class='col-sm-3' style='padding: 30px;'>";
+              echo "<div class='team'>";
+              echo "<div class='team-img'>";
+              echo "<img style='border-radius: 100%;' src="."image/Library-Team/".$pic.">";
+              echo "</div>";
+              echo "<div class='mname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['name']."</p>";
+              echo "</div>";
+              echo "<div class='pname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['teampost']."</p>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+              }
+              elseif($i%4==2){
+                echo "<div class='col-sm-3' style='padding: 30px;'>";
+              echo "<div class='team'>";
+              echo "<div class='team-img'>";
+              echo "<img style='border-radius: 100%;' src="."image/Library-Team/".$pic.">";
+              echo "</div>";
+              echo "<div class='mname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['name']."</p>";
+              echo "</div>";
+              echo "<div class='pname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['teampost']."</p>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+              }
+              else{
+                echo "<div class='col-sm-3' style='padding: 30px;'>";
+              echo "<div class='team'>";
+              echo "<div class='team-img'>";
+              echo "<img style='border-radius: 100%;' src="."image/Library-Team/".$pic.">";
+              echo "</div>";
+              echo "<div class='mname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['name']."</p>";
+              echo "</div>";
+              echo "<div class='pname'>";
+              echo "<p style="."font-family: 'Montserrat', sans-serif;".">".$row['teampost']."</p>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+              $f=0;
+              }
+            }
+            $i++;
+          }
+        }  
+      ?>
   </div>
 </section>
 <?php include("include/footerstrip.php"); ?>
